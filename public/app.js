7868f902399f8c144b9f97542c38ace0e009bb3c
John Chishugi <jkchishugi@gmail.com>
2022-08-21 15:49:52 +0200

    ft-final-touch

--- public/app.js
+++ public/app.js
@@ -13,12 +13,52 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
+// //GENERICS
+// const addID = <T extends object>(obj: T) => {
+//     let uid = Math.floor(Math.random() * 100);
+//     return { ...obj, uid};
+// }
+// let docOne = addID({name: 'Johgn', age: 33});
+// // let docTwo = addID('jjj');
+// console.log(docOne.name);
+// //ENUMS
+// enum ResourceType { BOOK, AUTHOR, DIRECTOR, PERSON }
+// //With Interfaces
+// interface Resource<T> {
+//     uid: number;
+//     resourceName: ResourceType;
+//     data: T;
+// };
+// const docThree: Resource<object> = {
+//     uid: 2,
+//     resourceName: ResourceType.AUTHOR,
+//     data: { name: 'shuam' }
+// }
+// const docFour: Resource<string[]> = {
+//     uid: 99,
+//     resourceName: ResourceType.DIRECTOR,
+//     data: ['ddd', 'ddd', 'g']
+// }
+// console.log(docThree, docFour);
+//tuples
+let arr = ['ray', 25, true];
+arr[0] = false;
+arr[1] = 'john';
+arr = [233, 'jg', false];
+let tup = ['jhon', 33, false];
+tup[0] = 'chishugi';
+tup[1] = 988;
+tup[2] = true;
+let student;
+student = ['John', 323232];
--- src/app.ts
+++ src/app.ts
@@ -19,15 +19,69 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
+    let values: [string, string, number];
+    values = [tofrom.value, details.value, amount.valueAsNumber]
     let doc: HasFormatter;
 
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     } else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
 
     list.render(doc, type.value, 'end');
 
 });
+
+// //GENERICS
+// const addID = <T extends object>(obj: T) => {
+//     let uid = Math.floor(Math.random() * 100);
+
+//     return { ...obj, uid};
+// }
+
+// let docOne = addID({name: 'Johgn', age: 33});
+// // let docTwo = addID('jjj');
+// console.log(docOne.name);
+
+// //ENUMS
+// enum ResourceType { BOOK, AUTHOR, DIRECTOR, PERSON }
+
+// //With Interfaces
+// interface Resource<T> {
+//     uid: number;
+//     resourceName: ResourceType;
+//     data: T;
+// };
+
+// const docThree: Resource<object> = {
+//     uid: 2,
+//     resourceName: ResourceType.AUTHOR,
+//     data: { name: 'shuam' }
+// }
+
+// const docFour: Resource<string[]> = {
+//     uid: 99,
+//     resourceName: ResourceType.DIRECTOR,
+//     data: ['ddd', 'ddd', 'g']
+// }
+
+// console.log(docThree, docFour);
+
+//tuples
+
+let arr = ['ray', 25, true];
+
+arr[0] = false;
+arr[1] = 'john';
+arr = [233, 'jg', false];
+
+let tup: [string, number, boolean] = ['jhon', 33, false];
+tup[0] = 'chishugi';
+tup[1] = 988;
+tup[2] = true;
+
+let student: [string, number];
+student = ['John', 323232];
+
 
